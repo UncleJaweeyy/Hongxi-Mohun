d64dd4c103b07baa9dcbd09b2d7b66af2c99a285
UncleJaweeyy <romanojohnreylumitap@gmail.com>
2026-04-26 01:29:04 +0800

    feat: add Firebase configuration for web integration

---
+++ js/shared/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseWebConfig = {
+    apiKey: "AIzaSyDS5xxWplNWcWOHuh-fPt53RQYN-Yul-sk",
+    authDomain: "hongxi-mohun.firebaseapp.com",
+    projectId: "hongxi-mohun",
+    storageBucket: "hongxi-mohun.firebasestorage.app",
+    messagingSenderId: "285758759593",
+    appId: "1:285758759593:web:f335388561c42e57d0bfb8",
+    measurementId: "G-RF7FJ9MTQB"
+};
